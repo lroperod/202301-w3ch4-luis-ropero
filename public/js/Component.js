5ce1d018b23c8acda59b30e3dc0e8a702699e865
luis <luisrym@gmail.com>
2023-02-02 19:20:02 +0100

    modify privacity component

--- public/js/Component.js
+++ public/js/Component.js
@@ -1,11 +1,26 @@
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _Component_element, _Component_parentElement;
 export default class Component {
     constructor(parentElement, tag, cssClass = '') {
-        this.element = document.createElement(tag);
-        this.element.className = cssClass;
-        this.parentElement = parentElement;
+        _Component_element.set(this, void 0);
+        _Component_parentElement.set(this, void 0);
+        __classPrivateFieldSet(this, _Component_element, document.createElement(tag), "f");
+        __classPrivateFieldGet(this, _Component_element, "f").className = cssClass;
+        __classPrivateFieldSet(this, _Component_parentElement, parentElement, "f");
     }
     render() {
         var _a;
-        (_a = this.parentElement) === null || _a === void 0 ? void 0 : _a.appendChild(this.element);
+        (_a = __classPrivateFieldGet(this, _Component_parentElement, "f")) === null || _a === void 0 ? void 0 : _a.appendChild(__classPrivateFieldGet(this, _Component_element, "f"));
     }
 }
+_Component_element = new WeakMap(), _Component_parentElement = new WeakMap();
--- src/components/Component.ts
+++ src/components/Component.ts
@@ -1,14 +1,14 @@
 export default class Component {
-  element: HTMLElement;
-  parentElement: HTMLElement | null;
+  #element: HTMLElement;
+  #parentElement: HTMLElement | null;
 
   constructor(parentElement: HTMLElement, tag: string, cssClass: string = '') {
-    this.element = document.createElement(tag);
-    this.element.className = cssClass;
-    this.parentElement = parentElement;
+    this.#element = document.createElement(tag);
+    this.#element.className = cssClass;
+    this.#parentElement = parentElement;
   }
 
   render() {
-    this.parentElement?.appendChild(this.element);
+    this.#parentElement?.appendChild(this.#element);
   }
 }
